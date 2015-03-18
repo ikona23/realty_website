460ccb333872bcc660bc25ee3809b51f4e0808c2
ikona23@gmail.com <ikona23@gmail.com>
2015-03-18 16:02:39 +1300

    first commit

---
+++ index.php
@@ -0,0 +1,506 @@
+<!DOCTYPE html>
+
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8" />
+
+  <!-- Set the viewport width to device width for mobile -->
+  <meta name="viewport" content="width=device-width" />
+
+  <title>Welcome to Peter's Reality</title>
+
+  <!-- CSS Files -->
+  <link rel="stylesheet" href="stylesheets/foundation.css">
+  <link rel="stylesheet" href="stylesheets/app.css">
+
+  <script src="javascripts/modernizr.foundation.js"></script>
+
+  <!-- IE Fix for HTML5 Tags -->
+  <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+</head>
+<body>
+
+	
+	<div class="grad">
+		<div class="row">
+			<div class="twelve columns">
+				<h2>Welcome to Peter's Reality</h2>
+					<p>Your Palmerston North Reality State Professional</p>
+				<hr>
+			</div>
+		</div>
+	</div>
+	
+	<!-- START MIDDLE SECTION !-->
+	<div class="row">
+		
+ 
+		<!-- START LEFT SECTION !-->
+		<div class="two columns">
+			<div class="row">
+				<div class="twelve columns">
+					<div class="panel h">
+						<h6>
+							Location
+						</h6>
+					</div>
+					<div class="panel menu">
+						<ul>
+							<li>Ashurst</li>
+							<li>Awapuni</li>
+							<li>Cloverlea</li>
+							<li>Feilding</li>
+							<li>Fitzherbert</li>
+							<li>Highbury</li>
+							<li>Hokowhitu</li>
+							<li>Kelvin Grove</li>
+							<li>Linton Military
+							Camp</li>
+							<li>Milson</li>
+							<li>Ohakea</li>
+							<li>Palmerston</li>
+							<li>North</li>
+							<li>Roslyn</li>
+							<li>Sanson</li>
+							<li>Takaro</li>
+							<li>Tangimoana</li>
+							<li>Terrace End</li>
+							<li>West End</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- END LEFT SECTION !-->
+		
+		
+		<!-- START MAIN SECTION !-->
+		<div class="eight columns">
+			<div class="row">
+				<div class="twelve columns">
+					<div class="row">
+						<div class="twelve columns">
+						
+							<div class="panel fil">
+								<div class="row">
+									<!-- 3 2 2 1 1 3 !-->
+									<div class="three columns">
+										<div class="twelve columns">
+											<label>From</label>
+												<select>
+													<option>$50,000</option>
+													<option>$100,000</option>
+													<option>$150,000</option>
+													<option>$200,000</option>
+												</select>
+										</div>
+									</div>
+									<div class="two columns">
+										<label>To:</label>
+										<select>
+											<option>$50,000</option>
+											<option>$100,000</option>
+											<option>$150,000</option>
+											<option>$200,000</option>
+										</select>
+									</div>
+									<div class="two columns">
+										<label>Bedrooms:</label>
+									</div>
+									<div class="one columns">
+										<label>Ensuite:</label>
+									</div>
+									<div class="one columns">
+										<label>Pool:</label>
+									</div>
+									<div class="two columns">
+										<label>Search:</label> <input id="search-box" type="text" placeholder="Search" />
+									</div>
+								</div>
+						    </div>
+						</div>
+					</div>
+						
+						
+					<div class="row">
+						<div class="four columns">
+							<div class="panel h">
+								<p>Suburb</p>
+							</div>
+							<div class="panel b">
+								<img src="http://placehold.it/168x90">
+								<p>
+									Street Address<br>
+									Bedrooms<br>
+									$000000
+								</p>
+							</div>
+						</div>
+						
+						
+						<div class="four columns">
+							<div class="panel h">
+								<p>Suburb</p>
+							</div>
+							<div class="panel b">
+								<img src="http://placehold.it/168x90">
+								<p>
+									Street Address<br>
+									Bedrooms<br>
+									$000000
+								</p>
+							</div>
+						</div>
+						
+						
+						<div class="four columns">
+							<div class="panel h">
+								<p>Suburb</p>
+							</div>
+							<div class="panel b">
+								<img src="http://placehold.it/168x90">
+								<p>
+									Street Address<br>
+									Bedrooms<br>
+									$000000
+								</p>
+							</div>
+						</div>
+
+					</div>
+						
+					<div class="row">
+					
+						<div class="four columns">
+							<div class="panel h">
+								<p>Suburb</p>
+							</div>
+							<div class="panel b">
+								<img src="http://placehold.it/168x90">
+								<p>
+									Street Address<br>
+									Bedrooms<br>
+									$000000
+								</p>
+							</div>
+						</div>
+						
+						
+						<div class="four columns">
+							<div class="panel h">
+								<p>Suburb</p>
+							</div>
+							<div class="panel b">
+								<img src="http://placehold.it/168x90">
+								<p>
+									Street Address<br>
+									Bedrooms<br>
+									$000000
+								</p>
+							</div>
+						</div>
+						
+						
+						<div class="four columns">
+							<div class="panel h">
+								<p>Suburb</p>
+							</div>
+							<div class="panel b">
+								<img src="http://placehold.it/168x90">
+								<p>
+									Street Address<br>
+									Bedrooms<br>
+									$000000
+								</p>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- END MAIN SECTION !-->
+		
+		
+		
+		
+		<!-- START RIGHT SECTION !-->
+		<div class="two columns">
+			<div class="row">
+				<div class="twelve columns">
+					<div class="panel nl">
+						<h6>
+							New Listings
+						</h6>
+					</div>
+				</div>
+			</div>
+			
+			
+			<div class="row">
+				<div class="twelve columns">
+					<div class="panel h">
+						<p>Suburb</p>
+					</div>
+					<div class="panel b">
+						<img src="http://placehold.it/78x39">
+						<p>
+							Street Address<br>
+							$000000
+						</p>
+					</div>
+				</div>
+			</div>
+			
+			
+			<div class="row">
+				<div class="twelve columns">
+					<div class="panel h">
+						<p>Suburb</p>
+					</div>
+					<div class="panel b">
+						<img src="http://placehold.it/78x39">
+						<p>
+							Street Address<br>
+							$000000
+						</p>
+					</div>
+				</div>
+			</div>
+			
+			
+			<div class="row">
+				<div class="twelve columns">
+					<div class="panel h">
+						<p>Suburb</p>
+					</div>
+					<div class="panel b">
+						<img src="http://placehold.it/78x39">
+						<p>
+							Street Address<br>
+							$000000
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- END RIGHT SECTION !-->
+	</div>
+	<!-- END MIDDLE SECTION !-->
+	
+	<!-- START FOOTER SECTION !-->
+	<div class="row">
+		<div class="panel">
+			<hr>
+			<p>
+				Powered by Peter's CMS &copy; 2012
+			</p>
+			<hr>
+		</div>
+	</div>
+	<!-- END FOOTER SECTION !-->
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+<!-- 		<div class="two columns">
+			<div class="panel h">
+				<p>Suburb</p>
+			</div>
+			<div class="panel b">
+			</div>
+		</div>
+		<div class="two columns">
+			<div class="panel h">
+				<p>Suburb</p>
+			</div>
+			<div class="panel b">
+			</div>
+		</div>
+		<div class="two columns">
+			<div class="panel h">
+				<p>Suburb</p>
+			</div>
+			<div class="panel b">
+			</div>
+		</div>
+		<div class="three columns">
+			<div class="panel h">
+				<p>Suburb</p>
+			</div>
+			<div class="panel b">
+			</div>
+		</div>
+	
+	
+	
+	
+		<div class="two columns">
+			<div class="panel h">
+				<p>Suburb</p>
+			</div>
+			<div class="panel b">
+			</div>
+		</div>
+		<div class="two columns">
+			<div class="panel h">
+				<p>Suburb</p>
+			</div>
+			<div class="panel b">
+			</div>
+		</div>
+		<div class="two columns">
+			<div class="panel h">
+				<p>Suburb</p>
+			</div>
+			<div class="panel b">
+			</div>
+		</div>
+		<div class="three columns">
+			<div class="panel h">
+				<p>Suburb</p>
+			</div>
+			<div class="panel b">
+			</div>
+		</div>
+	</div> -->
+	<!-- END -->
+	<!-- </div> -->
+
+
+  
+  
+  
+  
+  
+  
+  
+	
+	<!--
+	<div class="row">
+    <div class="eight columns">
+
+      <!-- MEOW -->
+	  
+      <!--<div class="row">
+        <div class="twelve columns">
+          <div class="panel radius">
+			<p>This is a twelve column section in a row. Each of these includes a div.panel element so you can see where the columns are - it's not required at all for the grid.</p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="six columns">
+          <div class="panel">
+            <p>Six columns</p>
+          </div>
+        </div>
+        <div class="six columns">
+          <div class="panel">
+            <p>Six columns</p>
+          </div>
+        </div>
+      </div>
+      
+
+      <h3>Tabs</h3>
+      <dl class="tabs">
+        <dd class="active"><a href="#simple1">Simple Tab 1</a></dd>
+        <dd><a href="#simple2">Simple Tab 2</a></dd>
+        <dd><a href="#simple3">Simple Tab 3</a></dd>
+      </dl>
+
+      <ul class="tabs-content">
+        <li class="active" id="simple1Tab">This is simple tab 1's content. Pretty neat, huh?</li>
+        <li id="simple2Tab">This is simple tab 2's content. Now you see it!</li>
+        <li id="simple3Tab">This is simple tab 3's content. It's, you know...okay.</li>
+      </ul>
+
+      <h3>Buttons</h3>
+
+      <div class="row">
+        <div class="six columns">
+          <p><a href="#" class="small button">Small Button</a></p>
+          <p><a href="#" class="button">Medium Button</a></p>
+          <p><a href="#" class="large button">Large Button</a></p>
+        </div>
+        <div class="six columns">
+          <p><a href="#" class="small alert button">Small Alert Button</a></p>
+          <p><a href="#" class="success button">Medium Success Button</a></p>
+          <p><a href="#" class="large secondary button">Large Secondary Button</a></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="four columns">
+      <h4>Getting Started</h4>
+      <p>We're stoked you want to try Foundation! To get going, this file (index.html) includes some basic styles you can modify, play around with, or totally destroy to get going.</p>
+
+      <h4>Other Resources</h4>
+      <p>Once you've exhausted the fun in this document, you should check out:</p>
+      <ul class="disc">
+        <li><a href="http://foundation.zurb.com/docs">Foundation Documentation</a><br />Everything you need to know about using the framework.</li>
+        <li><a href="http://github.com/zurb/foundation">Foundation on Github</a><br />Latest code, issue reports, feature requests and more.</li>
+        <li><a href="http://twitter.com/foundationzurb">@foundationzurb</a><br />Ping us on Twitter if you have questions. If you build something with this we'd love to see it (and send you a totally boss sticker).</li>
+      </ul>
+    </div>
+  </div>
+
+
+  
+  <!-- Included JS Files (Uncompressed) -->
+  <!--
+  
+  <script src="javascripts/modernizr.foundation.js"></script>
+  
+  <script src="javascripts/jquery.js"></script>
+  
+  <script src="javascripts/jquery.foundation.reveal.js"></script>
+  
+  <script src="javascripts/jquery.foundation.orbit.js"></script>
+  
+  <script src="javascripts/jquery.foundation.navigation.js"></script>
+  
+  <script src="javascripts/jquery.foundation.buttons.js"></script>
+  
+  <script src="javascripts/jquery.foundation.tabs.js"></script>
+  
+  <script src="javascripts/jquery.foundation.forms.js"></script>
+  
+  <script src="javascripts/jquery.foundation.tooltips.js"></script>
+  
+  <script src="javascripts/jquery.foundation.accordion.js"></script>
+  
+  <script src="javascripts/jquery.placeholder.js"></script>
+  
+  <script src="javascripts/jquery.foundation.alerts.js"></script>
+  
+  -->
+  
+  <!-- Included JS Files (Compressed) -->
+  <!--<script src="javascripts/foundation.js"></script>
+  
+  <!-- Initialize JS Plugins -->
+  <!--<script src="javascripts/app.js"></script>-->
+</body>
+</html>
